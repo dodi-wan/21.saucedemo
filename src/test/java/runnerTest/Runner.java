64017998651fd38ca3f050f776fb108f6d354a70
dodi-wan <dodsenergi@gmail.com>
2025-08-26 17:10:08 +0700

    update data

--- src/test/java/runnerTest/Runner.java
+++ src/test/java/runnerTest/Runner.java
@@ -2,20 +2,17 @@ package runnerTest;
 
 import org.junit.platform.suite.api.*;
 
-import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 
 //Runner test menggunakan Junit5 dengan anotasi Junit5
 //anotasi (masukan target lokasi folder di project)
-
 @Suite
 @IncludeEngines("cucumber")
-@IncludeTags("order")
+@IncludeTags("test")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.com.stepdef, hooks")
-//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@order") // Hanya menjalankan scenario dengan tag @Login
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm") // Laporan dalam format JSON
 @ConfigurationParameter(key = "allure.results.directory", value = "target/allure-results")
 
